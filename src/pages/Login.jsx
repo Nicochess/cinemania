e45c83504ec964ece228969f971aa7c226c4968e
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-01-03 22:50:13 -0300

    fix: login page (#3)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,45 +26,46 @@ const Login = () => {
   };
 
   return (
-    <main className="main__card">
-      <section className="card__profile">
-        <h1>Cinemania</h1>
-        <div className="icon--cinerama"></div>
-        <div className="alternative">
-          <h2>Faça Login</h2>
-          {error}
-        </div>
-        <form className="card__form" onSubmit={handleSubmit}>
-          <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            ref={emailRef}
-            required
-            placeholder="Insira seu e-mail"
-          />
-          <label htmlFor="password">Senha</label>
-          <input
-            type="password"
-            id="password"
-            ref={passwordRef}
-            required
-            placeholder="Insira sua senha"
-          />
-          <button type="submit" className="btn" disabled={loading}>
-            Fazer login
-          </button>
-        </form>
-      </section>
-      <section className="card__senha">
-        <div>
-          <Link to='/forgot'>Recuperar minha senha</Link>
-        </div>
-        <div className="criar__conta">
-          Deseja criar uma conta ? <Link to='/register'>Registre-se</Link>
-        </div>
-      </section>
-    </main>
+      <main className="main__card">
+        <section className="card__profile">
+          <h1>Cinemania</h1>
+          <div className="icon--cinerama"></div>
+          <div className="alternative">
+            <h2>Faça Login</h2>
+            {error}
+          </div>
+          <form className="card__form" onSubmit={handleSubmit}>
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              ref={emailRef}
+              required
+              placeholder="Insira seu e-mail"
+            />
+            <label htmlFor="password">Senha</label>
+            <input
+              type="password"
+              id="password"
+              ref={passwordRef}
+              required
+              placeholder="Insira sua senha"
+            />
+            <button type="submit" className="btn" disabled={loading}>
+              Fazer login
+            </button>
+          </form>
+        </section>
+        <section className="card__senha">
+          <div>
+            <Link to='/forgot'>Recuperar minha senha</Link>
+          </div>
+          <div className="criar__conta">
+            Deseja criar uma conta ? <Link to='/register'>Registre-se</Link>
+          </div>
+        </section>
+      </main>
+
   );
 };
 
