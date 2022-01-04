9dc72f74e4229ce0d024179f76c1eec601f14cb1
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-01-04 18:11:38 -0300

    feat: load more button implemented

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 
 const Add = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
+  const [index, setIndex] = useState(1);
 
   const handleChange = async (e) => {
     e.preventDefault();
@@ -17,13 +18,25 @@ const Add = () => {
       setResults(data.results);
     } else {
       setResults([]);
+      setIndex(1);
     }
   };
-  
-  const [index, setIndex] = useState(1)
-  const initialState = results.slice(0, index * 10)
-  const [list, setList] = useState(initialState)
 
+  const loadMore = async () => {
+    setIndex((prev) => {
+      return prev + 1;
+    });
+
+    const res = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${
+        process.env.REACT_APP_KEY
+      }&language=pt-BR&page=${index + 1}&include_adult=false&query=${query}`
+    );
+    const data = await res.json();
+    setResults((prev) => {
+      return [...prev, ...data.results];
+    });
+  };
 
   return (
     <div className="add-page">
@@ -39,15 +52,20 @@ const Add = () => {
           </div>
 
           {results.length > 0 && (
-            <ul className="results">
-              {results.map((movie) => {
-                return (
-                  <li key={movie.id}>
-                    <Card movie={movie} />
-                  </li>
-                );
-              })}
-            </ul>
+            <>
+              <ul className="results">
+                {results.map((movie) => {
+                  return (
+                    <li key={movie.id}>
+                      <Card movie={movie} />
+                    </li>
+                  );
+                })}
+              </ul>
+              <button className="btn" onClick={loadMore}>
+                Ver Mais
+              </button>
+            </>
           )}
         </div>
       </div>
