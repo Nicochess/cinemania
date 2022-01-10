d7e9952a9efadad00a80afb25c24887bda478ac9
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-01-10 14:43:14 -0300

    fix: card styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,27 @@
-import { Visibility } from "@material-ui/icons";
-import React from "react";
+import { MenuBook, Visibility } from "@material-ui/icons";
+import React, { useState } from "react";
+import Backdrop from "./Backdrop";
+import ModalCardInfo from "./ModalCardInfo";
 import MovieControls from "./MovieControls";
 
 const MovieCard = ({ movie, type }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="movie-card">
       <div className="overlay"></div>
 
       <img
-      className={type=== 'watched' ? 'img-watched' : 'img-watchlist'}
+        className={type === "watched" ? "img-watched" : "img-watchlist"}
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}
       />
+
+      <div className="controls">
+        <button className="btn btn-modal" onClick={() => setShowModal(true)}>
+          <MenuBook fontSize="small" />
+        </button>
+      </div>
 
       <MovieControls movie={movie} type={type} />
       {type === "watched" && (
@@ -19,6 +29,8 @@ const MovieCard = ({ movie, type }) => {
           <Visibility /> {movie && movie.watchedDate}
         </span>
       )}
+      {showModal && <Backdrop setShowModal={setShowModal} />}
+      {showModal && <ModalCardInfo setShowModal={setShowModal} movie={movie} />}
     </div>
   );
 };
