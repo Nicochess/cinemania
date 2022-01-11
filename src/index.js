ed70ed35926923418364656c8c4a604a214012d8
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-01-11 15:24:52 -0300

    feat: pwa install

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,4 +10,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.register()
+serviceWorker.register()
