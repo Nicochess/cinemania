66b591709c64e1a1f8f128fd0b2bc81a4d368748
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-01-17 14:09:36 -0300

    feat: modal with actions option

--- src/components/ModalCardInfo.jsx
+++ src/components/ModalCardInfo.jsx
@@ -1,7 +1,22 @@
-import { Close } from "@material-ui/icons";
-import React from "react";
+import { Close, PlaylistAdd, Visibility } from "@material-ui/icons";
+import GlobalContext from "../context/GlobalState";
+import { useContext } from "react";
 
 const ModalCardInfo = ({ movie, setShowModal }) => {
+  const { addToWatched, addToWatchlist, watchlist, watched } = useContext(GlobalContext);
+
+  let storedMovie = watchlist.find((item) => item.id === movie.id);
+
+  let storedMovieWatched = watched.find((item) => item.id === movie.id);
+
+  const watchListDisable = storedMovie
+    ? true
+    : storedMovieWatched
+    ? true
+    : false;
+
+  const watchedDisable = storedMovieWatched ? true : false;
+
   return (
     <article className="modal-info">
       <img
@@ -12,9 +27,25 @@ const ModalCardInfo = ({ movie, setShowModal }) => {
         <h1>{movie.title}</h1>
 
         <p>{movie.overview}</p>
-        <button className='btn' onClick={() => setShowModal(false)}>
+        <button className="btn btn-close" onClick={() => setShowModal(false)}>
           <Close />
         </button>
+        <div className="actions">
+          <button
+            className="btn"
+            disabled={watchListDisable}
+            onClick={() => addToWatchlist(movie)}
+          >
+            <PlaylistAdd /> Minha lista
+          </button>
+          <button
+            className="btn"
+            disabled={watchedDisable}
+            onClick={() => addToWatched(movie)}
+          >
+            <Visibility /> Assistidos
+          </button>
+        </div>
       </section>
     </article>
   );
